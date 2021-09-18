32ef26f6a284876ef5ca40e69c031c2efb33829d
Rahul Teja <rahulteja.a@hotmail.com>
2021-09-18 13:29:50 +0530

    Added Asset loaders

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import './styles.css';
+import IMAGE from './name.png';
+import LOGO from './test.svg';
+
 const App = () => {
     return (
-        <div>Hello</div>
+        <>
+            <div>Hello</div>
+            <img src={IMAGE} />
+            <img src={LOGO} />
+        </>
     )
 }
 
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -16,6 +16,18 @@ module.exports = {
                         loader: 'babel-loader'
                     }
                 ]
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg)$/,
+                type: 'asset/inline'
             }
         ]
     },
