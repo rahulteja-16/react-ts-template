3897483f1c09d9937daa14dddf6eb5ee5f2f2bfa
Rahul Teja <rahulteja.a@hotmail.com>
2021-09-18 12:57:34 +0530

    initial setup

---
+++ webpack/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: path.resolve(__dirname, '..', './src/index.tsx'),
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader'
+                    }
+                ]
+            }
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, '..', './build'),
+        filename: 'bundle.js',
+    },
+    mode: 'development',
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: path.resolve(__dirname, '..', './src/index.html'),
+        }),
+    ],
+}
