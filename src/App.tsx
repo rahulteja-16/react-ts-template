10732abebeb883e6080d226fa983e5a595bb1655
Rahul Teja <rahulteja.a@hotmail.com>
2021-09-18 15:22:43 +0530

    eslint and Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import './styles.css';
-import ClickCounter from './clickCOunter';
+import './styles.css'
+import ClickCounter from './ClickCounter'
 
 const App = () => {
     return (
         <>
             <div>sas HI</div>
             <ClickCounter />
-
         </>
     )
 }
 
-export default App;
+export default App
--- src/ClickCounter.tsx
+++ src/ClickCounter.tsx
@@ -1,12 +1,14 @@
-import { useState } from "react";
+import { useState } from 'react'
 
 const ClickCounter = () => {
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0)
     return (
         <div>
-            <button onClick={() => setCount(c => c + 1)}>Count {count} </button>
+            <button onClick={() => setCount((c) => c + 1)}>
+                Count {count}{' '}
+            </button>
         </div>
     )
 }
 
-export default ClickCounter;
+export default ClickCounter
