bc6a6901e013eaa0f0830eea1e6e05c698ff8d6a
Rahul Teja <rahulteja.a@hotmail.com>
2022-01-20 10:54:44 +0530

    Copy Plugin

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
-// const CopyPlugin = require('copy-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
 	entry: path.resolve(__dirname, '..', './src/index.tsx'),
@@ -40,12 +40,9 @@ module.exports = {
 		new HTMLWebpackPlugin({
 			template: path.resolve(__dirname, '..', './src/index.html'),
 		}),
-		new Copy({
+		new CopyPlugin({
 			patterns: [{ from: './public/*' }],
 		}),
-		// new CopyPlugin({
-		// 	patterns: [{ from: '../src', to: '/build' }],
-		// }),
 	],
 	stats: 'errors-only',
 }
